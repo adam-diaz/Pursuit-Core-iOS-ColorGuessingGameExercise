8a6e1866cffb5c9e6625e8742fb20e3744acd00a
Adán Diaz <adamdiaz@pursuit.org>
2019-10-29 16:19:53 -0400

    minor progress in color guessing game lab, may need help.

--- Color-Guessing-Game/Color-Guessing-Game/ViewController.swift
+++ Color-Guessing-Game/Color-Guessing-Game/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var randomColor: UIView!
+    
+//    let color = randomColor.
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+     let randNum = CGFloat.random(in: 0...1)
+    
+       let myColor = UIColor(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat)
+    
+   
+    
+    @IBAction func guessedColor(_ sender: UIButton) {
+        
+ 
+        
+        switch sender.tag {
+        case 0:
+            randomColor.backgroundColor = .red
+        case 1:
+            randomColor.backgroundColor =
+            .blue
+        case 2:
+            randomColor.backgroundColor = .green
+        default:
+            randomColor.backgroundColor = .black
+        }
+        
+    }
+    
 }
 
+//randomColor.backgroundColor = .red
