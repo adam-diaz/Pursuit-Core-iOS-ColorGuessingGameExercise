f6889317e0f2c774b4513f45aca6c69d438db32a
Adán Diaz <adamdiaz@pursuit.org>
2019-10-30 17:27:32 -0400

    made progress in the project

--- Color-Guessing-Game/Color-Guessing-Game/ViewController.swift
+++ Color-Guessing-Game/Color-Guessing-Game/ViewController.swift
@@ -13,18 +13,25 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayView: UIView!
     
-//    let color = randomColor.
+    @IBOutlet weak var scoreKeeper: UILabel!
     
+    @IBOutlet weak var highScoreKeeper: UILabel!
+    
+    //    let color = randomColor.
+    // score.increment() += 1
     
      let randNum = CGFloat.random(in: 0...1)
      // three random numbers for the rgb
      
-    var randomRed: CGFloat = 0
-    var randomGreen: CGFloat = 0
-    var randomBlue: CGFloat = 0
+    var randomRed: CGFloat = 0.0
+    var randomGreen: CGFloat = 0.0
+    var randomBlue: CGFloat = 0.0
 
-     
-
+    var score = 0
+    var highScore = 0
+//    var red: CGFloat = 0.0
+//    var green: CGFloat = 0.0
+//    var blue: CGFloat = 0.0
      
      
     
@@ -38,9 +45,7 @@ class ViewController: UIViewController {
         
         randomColor()
         
-        
 
-        
     }
 
     func randomColor() {
@@ -52,13 +57,12 @@ class ViewController: UIViewController {
     
     // the code below will be used for enabling and disabling game (do not FORGET)
     
-    @IBOutlet weak var red: UIButton!
-    @IBOutlet weak var blue: UIButton!
-    @IBOutlet weak var green: UIButton!
+//    @IBOutlet weak var red: UIButton!
+//    @IBOutlet weak var blue: UIButton!
+//    @IBOutlet weak var green: UIButton!
     
-    let rgbRed = UIColor.red
-    let rgbGreen = UIColor.green
-    let rgbBlue = UIColor.blue
+    // threre card monty is enable function to disable buttons look at the code from there
+//    
     
     @IBAction func guessedColor(_ sender: UIButton) {
 
@@ -67,16 +71,46 @@ class ViewController: UIViewController {
         switch sender.tag {
         case 0:
             
-            if rgbGreen == rgbBlue {
-                print("red")
+            if  randomRed >= randomGreen && randomRed >= randomBlue {
+               randomColor()
+                score += 1
+                scoreKeeper.text = "Score: \(score)"
+                if score > highScore {
+                    highScoreKeeper.text = "High Score: \(highScore)"
+                }
             }
+                
+                sender.isEnabled = false
+            
             
         case 1:
-            displayView.backgroundColor =
-            .blue
-        case 2:
-            displayView.backgroundColor = .green
-        default:
+            if randomBlue >= randomRed && randomBlue >= randomGreen {
+                randomColor()
+                score += 1
+                scoreKeeper.text = "Score: \(score)"
+                if score > highScore {
+                highScoreKeeper.text = "High Score: \(highScore)"
+            }
+            }
+               
+            sender.isEnabled = false
+            
+            
+         case 2:
+            if randomGreen >= randomRed && randomGreen >= randomBlue {
+                randomColor()
+                score += 1
+                scoreKeeper.text = "Score: \(score)"
+                if score > highScore {
+                highScoreKeeper.text = "High Score: \(highScore)"
+                
+            }
+                
+                sender.isEnabled = false
+            
+            }
+         default:
+                
             displayView.backgroundColor = .black
         }
         
@@ -84,17 +118,12 @@ class ViewController: UIViewController {
     
     @IBAction func newGame(_ sender: UIButton) {
        
-        let myColor = UIColor(red: CGFloat.random(in: 0...1), green:
-        
-            CGFloat.random(in: 0...1), blue: CGFloat.random(in: 0...1), alpha: 1)
-        
-        displayView.backgroundColor = myColor
+        randomColor()
         
 //        print(myColor)
     }
-    
-    
-}
+
 
 //randomColor.backgroundColor = .red
 // .max states whatever number or color is used the most
+}
